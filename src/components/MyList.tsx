57c64ca12e9b205f92c354ecf7dde4ccb70fc4c6
daf-github <daniel_fernandez_daf@yahoo.com.ar>
2024-02-06 20:46:00 -0300

    fetch data from RES API

--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -1,15 +1,35 @@
-import { FlatList, Text } from "react-native";
+import { ActivityIndicator, FlatList, Text } from "react-native";
 import character from "../data/character.json";
 import CharacterListItem from "./CharacterListItem";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const MyList = () => {
+  const [loading, setLoading] = useState(false);
+  const [items, setItems] = useState(character.results);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      setLoading(true);
+      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const responseJson = await response.json();
+
+      setItems(responseJson.results);
+      setLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <FlatList
-      data={character.results}
+      data={items}
       renderItem={({ item }) => <CharacterListItem character={item} />}
       contentContainerStyle={{
-        gap: 50,
+        gap: 10,
       }}
     />
   );
